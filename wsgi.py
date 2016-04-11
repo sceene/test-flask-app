11f61b83b699d863fa00fc47c4f49b109e771657
Steven Mahana <stevenmahana@gmail.com>
2016-04-11 16:39:23 -0700

    update test app

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,5 @@ from V1App import create_app
 app = create_app('config')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    #app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run()
