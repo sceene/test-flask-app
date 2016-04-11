a929ed66e211f48731c063954262a69ec7ce97cd
Steven Mahana <stevenmahana@gmail.com>
2016-04-10 18:29:24 -0700

    update test app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask inside Docker!!"
+    return "Sceene inside Docker!!"
 
 if __name__ == "__main__":
     #app.run(debug=True,host='0.0.0.0')
